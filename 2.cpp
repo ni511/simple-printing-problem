939d6980b6dcfd18615008801a57ac018066a9cc
ni511 <103868519+ni511@users.noreply.github.com>
2022-06-02 17:27:38 +0530

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -21,3 +21,33 @@ int main()
         i++;
     }
 }
+cout<<"****************************************************** NEXT PATTERN ******************************************************";
+//simple paattern 2
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i=1,n;
+    cout<<"n:\n";
+    cin>>n;
+    while(i<=n)
+    {
+        int j=1,value=i;/*OR if value is not use then 
+                          while(j<=i)
+                           {
+                             cout<<i-j+1;
+                                  j++;
+                           }
+                              */
+        while(j<=i)
+        {
+            cout<<value;
+            value--;
+            j++;
+            
+        }
+        cout<<endl;
+        i++;
+    }
+}
